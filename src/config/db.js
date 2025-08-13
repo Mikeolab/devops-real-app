32ce1b2adb015815d8d09ef1024c7607b132553d
Mikeolab <olowolabiakinwale@gmail.com>
2025-08-13 11:53:49 +0100

    remove old root src to ensure CI builds client/

--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-async function connectDB(uri) {
-  mongoose.set('strictQuery', true);
-  await mongoose.connect(uri);
-  return mongoose.connection;
-}
-
-module.exports = { connectDB };
--- src/server.js
+++
@@ -1,7 +0,0 @@
-// src/server.js
-const app = require('./app'); // <- app.js exports the app itself
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`API running on :${PORT}`);
-});
